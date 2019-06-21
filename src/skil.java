0feeec7b154e6f007d30c56532593be098909b8b
League Student <league@iMac-168.attlocal.net>
2019-06-20 19:55:20 -0700

    Create skil.java

---
+++ src/skil.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class skil {
+public static void main(String[] args) {
+String d =JOptionPane.showInputDialog(null, " How many dimes do you have");	
+	
+
+	int v=Integer.parseInt(d);
+	
+	JOptionPane.showMessageDialog(null, "You have "+v*1.4+"Rand");
+
+ String B =JOptionPane.showInputDialog("how tall are you(in inches)");
+
+ int g=Integer.parseInt(B)
+if (g<36) {
+	JOptionPane.showMessageDialog(null, "eat you wheatys");
+}
+
+}
+}
