15c10be994f673419953306c88fa96163638c2f6
Airsoft102 <42328862+Airsoft102@users.noreply.github.com>
2019-08-15 21:12:01 -0700

    update of fun

--- src/fun.java
+++ src/fun.java
@@ -6,18 +6,28 @@ public static void main(String[] args) {
 	String I= JOptionPane.showInputDialog("put in you price for this item");
 	
 	int g=Integer.parseInt(I);
-		
-	if (g<500) {
+		 int H=500;
+	if (g<H) {
 		JOptionPane.showMessageDialog(null, "Best Price!");
+	H=g;
 	}
 	else {
 		JOptionPane.showMessageDialog(null, "Better prices.");
 	}
 
+	String n=JOptionPane.showInputDialog("put in your price");
+	
+	int c=Integer.parseInt(n);
+
+if (g<H) {
+	JOptionPane.showMessageDialog(null, "Best Price!");
+H=g;
+}
+else {
+	JOptionPane.showMessageDialog(null, "Better prices.");
 }
 	
-	
-	
+}	
 }
 
 //this is for home fun
